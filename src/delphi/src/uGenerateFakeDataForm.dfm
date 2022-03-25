b60d9375437ef642670ba1ef2832fc39bce23bd0
lxxxv <justcodeit.py@gmail.com>
2022-03-25 17:44:51 +0900

    -

--- src/delphi/src/uGenerateFakeDataForm.dfm
+++ src/delphi/src/uGenerateFakeDataForm.dfm
@@ -2,8 +2,8 @@ object GenerateFakeDataForm: TGenerateFakeDataForm
   Left = 0
   Top = 0
   Caption = #49892#54665
-  ClientHeight = 399
-  ClientWidth = 711
+  ClientHeight = 503
+  ClientWidth = 645
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -12,15 +12,81 @@ object GenerateFakeDataForm: TGenerateFakeDataForm
   Font.Style = []
   OldCreateOrder = False
   Position = poScreenCenter
+  OnCreate = FormCreate
+  OnDestroy = FormDestroy
+  DesignSize = (
+    645
+    503)
   PixelsPerInch = 96
   TextHeight = 13
+  object lblResultFolderPath: TLabel
+    Left = 8
+    Top = 395
+    Width = 72
+    Height = 13
+    Caption = #44208#44284' '#54260#45908' '#44221#47196
+  end
+  object lblRows: TLabel
+    Left = 8
+    Top = 96
+    Width = 34
+    Height = 13
+    Caption = '#Rows'
+  end
+  object lblFormat: TLabel
+    Left = 192
+    Top = 96
+    Width = 44
+    Height = 13
+    Caption = 'lblFormat'
+  end
   object btnExecute: TButton
     Left = 8
-    Top = 8
-    Width = 121
-    Height = 81
-    Caption = 'btnExecute'
+    Top = 432
+    Width = 629
+    Height = 57
+    Anchors = [akLeft, akTop, akRight]
+    Caption = #49373#49457
     TabOrder = 0
     OnClick = btnExecuteClick
+  end
+  object edtresultfolderpath: TEdit
+    Left = 100
+    Top = 392
+    Width = 537
+    Height = 21
+    Anchors = [akLeft, akTop, akRight]
+    TabOrder = 1
+  end
+  object btnFileSelect: TButton
+    Left = 8
+    Top = 8
+    Width = 629
+    Height = 57
+    Anchors = [akLeft, akTop, akRight]
+    Caption = 'dataset '#54028#51068' '#49440#53469
+    TabOrder = 2
+    OnClick = btnFileSelectClick
+  end
+  object cbFormat: TComboBox
+    Left = 264
+    Top = 93
+    Width = 145
+    Height = 21
+    ItemIndex = 0
+    TabOrder = 3
+    Text = 'json'
+    OnKeyPress = cbFormatKeyPress
+    Items.Strings = (
+      'json')
+  end
+  object edtrows: TEdit
+    Left = 60
+    Top = 93
+    Width = 101
+    Height = 21
+    Anchors = [akLeft, akTop, akRight]
+    TabOrder = 4
+    OnKeyPress = edtrowsKeyPress
   end
 end
--- src/delphi/src/uGenerateFakeDataForm.pas
+++ src/delphi/src/uGenerateFakeDataForm.pas
@@ -7,13 +7,35 @@ uses
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
+  Texceptcheckmode = (eckselctfile, ectexecute);
+  Texceptcheckmodes = set of Texceptcheckmode;
+
   TGenerateFakeDataForm = class(TForm)
     btnExecute: TButton;
+    edtresultfolderpath: TEdit;
+    lblResultFolderPath: TLabel;
+    lblRows: TLabel;
+    lblFormat: TLabel;
+    btnFileSelect: TButton;
+    cbFormat: TComboBox;
+    edtrows: TEdit;
     procedure btnExecuteClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure btnFileSelectClick(Sender: TObject);
+    procedure cbFormatKeyPress(Sender: TObject; var Key: Char);
+    procedure edtrowsKeyPress(Sender: TObject; var Key: Char);
   private
     { Private declarations }
+    Ffileselectlist : TStringList;
+    Fmaxrow         : int64;
+    function execeptcheck(_exceptcheckmodes : Texceptcheckmodes): boolean;
+    function selectfiles: boolean;
+    function execute: boolean;
+    function checkmaxows: boolean;
   public
     { Public declarations }
+    property fileselectlist : TStringList read Ffileselectlist;
   end;
 
 var
@@ -24,20 +46,172 @@ implementation
 {$R *.dfm}
 
 procedure TGenerateFakeDataForm.btnExecuteClick(Sender: TObject);
+begin
+  if self.execeptcheck([eckselctfile, ectexecute]) then
+  begin
+    self.execute;
+  end;
+end;
+
+procedure TGenerateFakeDataForm.btnFileSelectClick(Sender: TObject);
+begin
+  if self.execeptcheck([eckselctfile]) then
+  begin
+    self.selectfiles;
+    self.checkmaxows;
+  end;
+end;
+
+procedure TGenerateFakeDataForm.cbFormatKeyPress(Sender: TObject;
+  var Key: Char);
+begin
+  Key := #0;
+end;
+
+function TGenerateFakeDataForm.checkmaxows: boolean;
+var
+  idx : longint;
+  pstringlist : tstringlist;
+begin
+  idx := 0;
+  while idx < self.Ffileselectlist.Count do
+  begin
+    pstringlist := tstringlist.Create;
+    pstringlist.LoadFromFile(self.Ffileselectlist.Strings[idx], Tencoding.UTF8);
+    if strtoint(self.edtrows.Text) <= pstringlist.Count then
+    begin
+      self.edtrows.Text := inttostr(pstringlist.Count);
+      self.Fmaxrow      := pstringlist.Count;
+    end;
+    pstringlist.free;
+    inc(idx);
+  end;
+end;
+
+procedure TGenerateFakeDataForm.edtrowsKeyPress(Sender: TObject; var Key: Char);
+begin
+  if (Key in ['0'..'9', #3, #8, #10, #13, #22]) then
+  begin
+    if (Key in [#8]) then
+    begin
+      if length(self.edtrows.Text) = 1 then
+      begin
+        Key := #0;
+      end;
+    end else
+    begin
+      if (strtointdef(self.edtrows.Text + Key, self.Fmaxrow) < self.Fmaxrow) then
+      begin
+
+      end else
+      begin
+        Key := #0;
+      end;
+    end;
+  end else
+  begin
+    Key := #0;
+  end;
+end;
+
+function TGenerateFakeDataForm.execeptcheck(_exceptcheckmodes : Texceptcheckmodes): boolean;
+begin
+  result := false;
+
+  if eckselctfile in _exceptcheckmodes  then
+  begin
+    result := false;
+
+    if directoryexists(self.edtresultfolderpath.Text) then
+    begin
+      result := true;
+    end else
+    begin
+      //
+      // 결과 파일 저장할 디렉토리 없음
+      //
+      Messagebox(self.handle, '결과 파일을 저장할 디렉토리가 정상적이지 않습니다.', pchar(self.Caption), MB_OK);
+      exit;
+    end;
+  end;
+
+  if ectexecute in _exceptcheckmodes  then
+  begin
+    result := false;
+
+    if self.Ffileselectlist.Count = 0 then
+    begin
+      Messagebox(self.Handle, '조합할 데이터의 원본 dataset을 선택하여 주십시오.', pchar(self.Caption), MB_OK);
+      exit;
+    end else
+    begin
+      result := true;
+    end;
+  end;
+end;
+
+function TGenerateFakeDataForm.execute: boolean;
+begin
+  TThread.CreateAnonymousThread
+  (
+    procedure
+    var
+      idx : longint;
+    begin
+      idx := 0;
+      while idx < self.Ffileselectlist.Count do
+      begin
+        inc(idx);
+      end;
+    end
+  ).Start;
+end;
+
+procedure TGenerateFakeDataForm.FormCreate(Sender: TObject);
+begin
+  self.Ffileselectlist          := TStringList.Create;
+  self.edtresultfolderpath.Text := extractfilepath(paramstr(0));
+  self.Fmaxrow                  := 0;
+  self.edtrows.Text             := inttostr(self.Fmaxrow);
+end;
+
+procedure TGenerateFakeDataForm.FormDestroy(Sender: TObject);
+begin
+  self.Ffileselectlist.free;
+end;
+
+function TGenerateFakeDataForm.selectfiles: boolean;
 var
   pFileOpenDialog : TFileOpenDialog;
   pFileTypeItem : TFileTypeItem;
+  idx : longint;
 begin
+  result := false;
+
   pFileOpenDialog := TFileOpenDialog.Create(nil);
   try
-    pFileOpenDialog.Title := 'dataset';
-    pFileOpenDialog.Options := pFileOpenDialog.Options + [fdoAllowMultiSelect];
-    pFileOpenDialog.DefaultFolder := ExtractFilePath(ParamStr(0));
-    pFileTypeItem := pFileOpenDialog.FileTypes.Add;
-    pFileTypeItem.DisplayName := 'dataset';
-    pFileTypeItem.FileMask := '*.'+pFileTypeItem.DisplayName;
-    if pFileOpenDialog.Execute then
-    begin
+    try
+      pFileOpenDialog.Title := 'select multi datasets';
+      pFileOpenDialog.Options := pFileOpenDialog.Options + [fdoAllowMultiSelect];
+      pFileOpenDialog.DefaultFolder := ExtractFilePath(ParamStr(0));
+      pFileTypeItem := pFileOpenDialog.FileTypes.Add;
+      pFileTypeItem.DisplayName := 'dataset';
+      pFileTypeItem.FileMask := '*.'+pFileTypeItem.DisplayName;
+
+      if pFileOpenDialog.Execute then
+      begin
+        self.Ffileselectlist.clear;
+
+        idx := 0;
+        while idx < pFileOpenDialog.Files.Count do
+        begin
+          self.Ffileselectlist.Add(pFIleOpenDialog.Files.Strings[idx]);
+          inc(idx);
+        end;
+
+        result := true;
+      end;
+    except
 
     end;
   finally
